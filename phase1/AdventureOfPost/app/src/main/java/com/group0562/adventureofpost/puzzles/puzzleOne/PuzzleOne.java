ad979e557ce08807c903566e37e1296fbe38c499
Le Rong Shi <larry.shi@mail.utoronto.ca>
2019-10-25 22:27:27 -0400

    added implementation of abstract method for puzzleOne

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/puzzles/puzzleOne/PuzzleOne.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/puzzles/puzzleOne/PuzzleOne.java
@@ -3,4 +3,18 @@ package com.group0562.adventureofpost.puzzles.puzzleOne;
 import com.group0562.adventureofpost.puzzles.Puzzles;
 
 public class PuzzleOne extends Puzzles {
+
+    public PuzzleOne() {
+        super(1000000000);
+    }
+
+    @Override
+    public void updatePoints() {
+
+    }
+
+    @Override
+    public void checkComplete() {
+
+    }
 }
