193e6a4a356f8b67d91cc43238c1554b10c59ee0
wanglemi <johnsmith.wang@mail.utoronto.ca>
2019-10-26 10:39:06 +0800

    created override methods and constructor of puzzle 3

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/puzzles/puzzleThree/PuzzleThree.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/puzzles/puzzleThree/PuzzleThree.java
@@ -3,4 +3,18 @@ package com.group0562.adventureofpost.puzzles.puzzleThree;
 import com.group0562.adventureofpost.puzzles.Puzzles;
 
 public class PuzzleThree extends Puzzles {
+
+    public PuzzleThree(long time) {
+        super(time);
+    }
+
+    @Override
+    public void updatePoints() {
+
+    }
+
+    @Override
+    public void checkComplete() {
+
+    }
 }
