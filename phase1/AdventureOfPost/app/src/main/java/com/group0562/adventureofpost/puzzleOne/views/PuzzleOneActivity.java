fa69fac0305e59b56350879318d7df85f9b79768
Le Rong Shi <larry.shi@mail.utoronto.ca>
2019-10-26 14:02:01 -0400

    conflict test

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/puzzleOne/views/PuzzleOneActivity.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/puzzleOne/views/PuzzleOneActivity.java
@@ -3,4 +3,8 @@ package com.group0562.adventureofpost.puzzleOne.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PuzzleOneActivity extends AppCompatActivity {
+
+    public PuzzleOneActivity() {
+
+    }
 }
