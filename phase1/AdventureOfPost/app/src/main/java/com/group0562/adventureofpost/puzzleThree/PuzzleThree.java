bb5a39a370e5dc268a393f6487f6d47a5287837b
Matthew Yang <matty.yang@mail.utoronto.ca>
2019-10-26 16:22:20 -0400

    finish constructor of puzzle 3, setBallLocation of puzzle 3

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/puzzleThree/PuzzleThree.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/puzzleThree/PuzzleThree.java
@@ -2,16 +2,27 @@ package com.group0562.adventureofpost.puzzleThree;
 
 import com.group0562.adventureofpost.Puzzles;
 
-public class PuzzleThree extends Puzzles {
 
+public class PuzzleThree extends Puzzles {
+    /* bound indexes follow from left, right, up and down */
     private static double[] bound;
     private double center_x;
     private double center_y;
-    private int r;
+    private double r;
     private int points;
 
-    public PuzzleThree(long time) {
+    public PuzzleThree(long time, double radius, double[] bounds) {
+
         super(time);
+        this.points = 0;
+        this.r = radius;
+        this.setBallLocation();
+        PuzzleThree.bound = bounds;
+    }
+
+    private void setBallLocation(){
+        this.center_x = Math.random()*(PuzzleThree.bound[1]-2*this.r) + this.r;
+        this.center_y = Math.random()*(PuzzleThree.bound[3]-2*this.r) + this.r;
     }
 
     @Override
