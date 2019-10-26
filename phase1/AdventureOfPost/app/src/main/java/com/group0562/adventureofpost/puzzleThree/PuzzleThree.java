c96b0fa0acde9d260ebdfaf8c6cf241ff0a2a0d5
wanglemi <johnsmith.wang@mail.utoronto.ca>
2019-10-27 04:04:43 +0800

    created instance variables for puzzle 3

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/puzzleThree/PuzzleThree.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/puzzleThree/PuzzleThree.java
@@ -4,6 +4,12 @@ import com.group0562.adventureofpost.Puzzles;
 
 public class PuzzleThree extends Puzzles {
 
+    private static double[] bound;
+    private double center_x;
+    private double center_y;
+    private int r;
+    private int points;
+
     public PuzzleThree(long time) {
         super(time);
     }
