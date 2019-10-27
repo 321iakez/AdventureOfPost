968148a3d6c3d99ae3c707d2acbaa6bb3e477e5e
Le Rong Shi <larry.shi@mail.utoronto.ca>
2019-10-27 00:25:38 -0400

    added sudoku puzzle data and constructors for board class

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/sudoku/Board.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/sudoku/Board.java
@@ -1,4 +1,57 @@
 package com.group0562.adventureofpost.sudoku;
 
 public class Board {
+
+    /**
+     * The number of rows the board has.
+     */
+    private int rows;
+
+    /**
+     * The number of columns the board has.
+     */
+    private int cols;
+
+    /**
+     * A 2-D array of Cell instances representing the virtual board.
+     */
+    private Cell[][] board;
+
+    /**
+     * Default constructor.
+     */
+    public Board() {
+        rows = 6;
+        cols = 6;
+        board = new Cell[6][6];
+    }
+
+    /**
+     * Constructor with preloaded board.
+     *
+     * @param board  the 2-D array representing the preloaded board.
+     * @param rowDim the number of rows of the preloaded board.
+     * @param colDim the number of columns of the preloaded board.
+     */
+    public Board(int[][] board, int rowDim, int colDim) {
+        rows = rowDim;
+        cols = colDim;
+        this.board = new Cell[rowDim][colDim];
+
+        loadBoard(board);
+    }
+
+    /**
+     * Loads a given board as current game board.
+     *
+     * @param board a 2-D int array representing the values of each cell.
+     */
+    private void loadBoard(int[][] board) {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                Cell cell = new Cell(board[row][col], board[row][col] == 0);
+                this.board[row][col] = cell;
+            }
+        }
+    }
 }
