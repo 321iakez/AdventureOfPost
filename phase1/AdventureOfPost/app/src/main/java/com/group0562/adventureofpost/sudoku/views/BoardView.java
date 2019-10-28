2d518a3141ffd4dbc7fcaf4b482a06eeecbb342d
Le Rong Shi <larry.shi@mail.utoronto.ca>
2019-10-28 15:23:20 -0400

    minor update

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/sudoku/views/BoardView.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/sudoku/views/BoardView.java
@@ -3,6 +3,7 @@ package com.group0562.adventureofpost.sudoku.views;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.GridLayout;
 
 public class BoardView extends GridLayout {
@@ -24,5 +25,16 @@ public class BoardView extends GridLayout {
 
     private void initBoardView(Context context) {
         setColumnCount(BOARD_SIZE);
+
+        // Set overall grid layout
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(-1, -1);
+        params.setMargins(1, 1, 1, 1);
+
+        // Put tiles to the grid and assign each tile to a button
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                Button button = new Button(getContext());
+            }
+        }
     }
 }
