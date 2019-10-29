ba28b8eb4e3b93345f6caa31e55ab2b4edc66570
danie <dydaniel.xu@mail.utoronto.ca>
2019-10-28 21:02:15 -0400

    Had SudokuActivity run when user press the 'login' button

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/MainActivity.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/MainActivity.java
@@ -3,11 +3,18 @@ package com.group0562.adventureofpost;
 import android.os.Bundle;
 import android.view.View;
 
+import android.content.Intent;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.group0562.adventureofpost.sudoku.views.SudokuActivity;
 
 import java.sql.SQLOutput;
 
 public class MainActivity extends AppCompatActivity {
+    //TODO i dont know if this is correct
+    public final static String EXTRA_MESSAGE = "com.group0562.AdventureOfPost.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +22,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    //TODO Currently will bring user to the SudokuActivity screen
     public void sendMessage(View view) {
-        System.out.println("button works");
+        Intent intent = new Intent(this, SudokuActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
