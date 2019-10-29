2792075bf6d0fc0807cf191aacdffff6edeb05e3
danie <dydaniel.xu@mail.utoronto.ca>
2019-10-28 20:22:51 -0400

    Added handler method in MainActivity for 'login' Button displayed on device

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/MainActivity.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/MainActivity.java
@@ -1,8 +1,11 @@
 package com.group0562.adventureofpost;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.SQLOutput;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void sendMessage(View view) {
+        System.out.println("button works");
     }
 }
