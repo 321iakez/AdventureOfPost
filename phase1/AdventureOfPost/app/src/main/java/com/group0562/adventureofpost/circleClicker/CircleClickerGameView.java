0cd2b11cf71e05324021e6e420c9f1d6017fc4d0
Matthew Yang <matty.yang@mail.utoronto.ca>
2019-10-28 22:43:53 -0400

    first step modifying circleclickergameview and circle class

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/circleClicker/CircleClickerGameView.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/circleClicker/CircleClickerGameView.java
@@ -1,4 +1,27 @@
 package com.group0562.adventureofpost.circleClicker;
 
-public class CircleClickerGameView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CircleClickerGameView extends View {
+    private Paint paint;
+    private static final float Stroke_Thickness = 3;
+
+    public CircleClickerGameView(Context context, @Nullable AttributeSet attrs){
+        super(context, attrs);
+        this.paint = new Paint();
+        this.paint.setColor(Color.BLACK);
+        this.paint.setStrokeWidth(CircleClickerGameView.Stroke_Thickness);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.CYAN);
+    }
 }
