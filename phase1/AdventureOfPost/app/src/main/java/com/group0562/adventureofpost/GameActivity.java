fe2dc94fe8b5b2d8a8a43d36177e677e0f018a71
danie <dydaniel.xu@mail.utoronto.ca>
2019-10-29 14:57:11 -0400

    Added functions to 3 buttons in GameActivity Screen

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/GameActivity.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/GameActivity.java
@@ -2,13 +2,46 @@ package com.group0562.adventureofpost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.group0562.adventureofpost.circleClicker.views.CircleClickerActivity;
+import com.group0562.adventureofpost.sudoku.views.SudokuActivity;
+import com.group0562.adventureofpost.trivia.views.TriviaActivity;
 
 public class GameActivity extends AppCompatActivity {
+    //TODO don't know if this is correct
+    public final static String EXTRA_MESSAGE = "com.group0562.AdventureOfPost.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void onClickCircleClicker(View view) {
+        Intent intent = new Intent(this, CircleClickerActivity.class);
+        TextView textView = (TextView) findViewById(R.id.CCButton);
+        String message = textView.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void onClickSudoku(View view) {
+        Intent intent = new Intent(this, SudokuActivity.class);
+        TextView textView = (TextView) findViewById(R.id.SudokuButton);
+        String message = textView.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void onClickTrivia(View view) {
+        Intent intent = new Intent(this, TriviaActivity.class);
+        TextView textView = (TextView) findViewById(R.id.TriviaButton);
+        String message = textView.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
