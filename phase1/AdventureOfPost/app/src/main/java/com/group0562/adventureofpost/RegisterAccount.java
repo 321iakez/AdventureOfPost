cb658b5c9ffb352bd3b7b08a6c04f86197101070
huangzee <brianhuang1128@gmail.com>
2019-10-30 23:01:32 -0400

    cleaned up RegisterAccount.java, removed unecessary method.

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/RegisterAccount.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/RegisterAccount.java
@@ -52,30 +52,5 @@ public class RegisterAccount extends AppCompatActivity {
             }
         });
     }
-
-    public void onClickRegister(View view) {
-        String username, password, securityQuestion, answer;
-        EditText txtUsername = findViewById(R.id.username);
-        username = txtUsername.getText().toString();
-        EditText txtPassword = findViewById(R.id.password);
-        password = txtPassword.getText().toString();
-        EditText txtSecurityQ = findViewById(R.id.securityQuestion);
-        securityQuestion = txtSecurityQ.getText().toString();
-        EditText txtAnswer = findViewById(R.id.answer);
-        answer = txtAnswer.getText().toString();
-
-
-        //TODO this is just to check should be removed soon
-        System.out.println("Account Username: " + username);
-        System.out.println("Account Password: " + password);
-        System.out.println("Account Security Question: " + securityQuestion);
-        System.out.println("Account Answer: " + answer);
-
-        //TODO Uncomment lines to have register button take user back to login screen
-        //Intent intent = new Intent(this, MainActivity.class);
-        //startActivity(intent);
-
-
     }
 
-}
