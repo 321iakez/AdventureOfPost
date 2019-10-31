80c16f5f0478630f3a1d4277bdfe8e99111d9df6
Your Name <brianhuang1128@gmail.com>
2019-10-30 22:34:48 -0400

    test

--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/MainActivity.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.content.Intent;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +13,21 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     //TODO I dont know if this is correct
     public final static String EXTRA_MESSAGE = "com.group0562.AdventureOfPost.MESSAGE";
+    EditText userName, passWord;
+    Button register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register = (Button)findViewById(R.id.button2);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                onClickRegisterNew(v);
+            }
+        });
     }
 
     public void onClickLogin(View view){
--- phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/RegisterAccount.java
+++ phase1/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/RegisterAccount.java
@@ -7,14 +7,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.group0562.adventureofpost.trivia.DatabaseHelper;
 
 public class RegisterAccount extends AppCompatActivity {
-
+    EditText registerUser, registerPass, registercPass;
+    Button registerButton;
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
+
+        registerUser = findViewById(R.id.username);
+        registerPass = findViewById(R.id.password);
+        registerButton = findViewById(R.id.registerButton);
+        registercPass = findViewById(R.id.cPass);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newUser = registerUser.getText().toString();
+                String newPass = registerPass.getText().toString();
+                String cPass = registercPass.getText().toString();
+                if(newUser.equals("")||newPass.equals("")||cPass.equals("")){
+                    Toast.makeText(getApplicationContext(), "Fields are Empty", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(newPass.equals(cPass)){
+                        Boolean checkmail = db.checkMail(newUser);
+                        if(checkmail){
+                            Boolean insert = db.insert(newUser, newPass);
+                            if(insert == true){
+                                Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Email Already Exists", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+            }
+        });
     }
 
     public void onClickRegister(View view) {
