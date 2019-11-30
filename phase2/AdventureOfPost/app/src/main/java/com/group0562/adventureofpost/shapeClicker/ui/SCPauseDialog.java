ba5124b4296e12be1f63e632e337dc6aa3830a71
Matthew Yang <matty.yang@mail.utoronto.ca>
2019-11-30 16:49:51 -0500

    except for resume and save everything in sc class works

--- phase2/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/shapeClicker/ui/SCPauseDialog.java
+++ phase2/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/shapeClicker/ui/SCPauseDialog.java
@@ -20,14 +20,14 @@ public class SCPauseDialog extends AppCompatDialogFragment {
     private final String RETURN_SAVE = "RETURN_SAVE";
     private final String RESUME = "RESUME";
 
-    private SCPauseDialog.PauseDialogListener listener;
+    private SCPauseDialog.SCPauseDialogListener listener;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.dialog_sudoku_pause, null);
+        View view = inflater.inflate(R.layout.dialog_sc_pause, null);
 
         builder.setView(view)
                 .setTitle("")
@@ -41,13 +41,13 @@ public class SCPauseDialog extends AppCompatDialogFragment {
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         try {
-            listener = (SCPauseDialog.PauseDialogListener) context;
+            listener = (SCPauseDialog.SCPauseDialogListener) context;
         } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString() + "Must implement PauseDialogListener");
+            throw new ClassCastException(context.toString() + "Must implement SCPauseDialogListener");
         }
     }
 
-    interface PauseDialogListener {
+    interface SCPauseDialogListener {
         void saveGame(String mode);
     }
 }
