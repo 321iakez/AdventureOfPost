aa74935f0edca7e9c557ac1d411826f012d0ec90
Le Rong Shi <larry.shi@mail.utoronto.ca>
2019-11-30 14:53:59 -0500

    removed level for sudoku

--- phase2/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/sudoku/ui/SudokuStartActivity.java
+++ phase2/AdventureOfPost/app/src/main/java/com/group0562/adventureofpost/sudoku/ui/SudokuStartActivity.java
@@ -14,11 +14,9 @@ public class SudokuStartActivity extends AppCompatActivity {
 
     private final String[] GRID_OPTIONS = new String[]{"6x6", "9x9"};
     private final String[] DIFFICULTY_OPTIONS = new String[]{"Easy", "Medium", "Hard"};
-    private final Integer[] LEVEL_OPTIONS = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     private Spinner gridSize;
     private Spinner difficulty;
-    private Spinner level;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +33,6 @@ public class SudokuStartActivity extends AppCompatActivity {
                 R.layout.support_simple_spinner_dropdown_item, DIFFICULTY_OPTIONS);
         difficulty.setAdapter(difficultyOptionAdaptor);
 
-        level = findViewById(R.id.levelSelect);
-        ArrayAdapter<Integer> levelOptionAdaptor = new ArrayAdapter<>(this,
-                R.layout.support_simple_spinner_dropdown_item, LEVEL_OPTIONS);
-        level.setAdapter(levelOptionAdaptor);
-
         findViewById(R.id.startButton).setOnClickListener(this::onClickStart);
     }
 
@@ -47,7 +40,6 @@ public class SudokuStartActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SudokuActivity.class);
         intent.putExtra("gridSize", gridSize.getSelectedItem().toString());
         intent.putExtra("difficulty", difficulty.getSelectedItem().toString());
-        intent.putExtra("level", (Integer) level.getSelectedItem());
         startActivity(intent);
     }
 }
